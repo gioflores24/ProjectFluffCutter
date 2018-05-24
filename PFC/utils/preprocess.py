60dd771972cb4eb8c3d3ccd4b8fcf140acc651f8
giovanniflores <gdr943@gmail.com>
2018-05-24 14:22:50 -0700

    Added project

---
+++ PFC/utils/preprocess.py
@@ -0,0 +1,8 @@
+from docx import Document
+
+def getText(filename):
+    doc = Document(filename)
+    fullText = []
+    for para in doc.paragraphs:
+        fullText.append(para.text)
+    return '\n'.join(fullText)
