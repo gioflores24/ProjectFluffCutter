5bf12fb4f68f8e262da218f0955ca10c611297d3
Giovanni Flores <gdr943@gmail.com>
2018-05-29 11:58:43 -0700

    added methods to utils

--- PFC/playground/sandbox0.py
+++ PFC/playground/sandbox0.py
@@ -1,6 +1,13 @@
-from nltk import *
+from nltk.book import text4
+from nltk.draw.dispersion import dispersion_plot
 from matplotlib import *
 from numpy import *
 
 text = "The citizens of democracy are rejoicing in freedom and their duties are ok"
+
+
+
+
+
+
 
--- PFC/utils/preprocess.py
+++ PFC/utils/preprocess.py
@@ -6,3 +6,12 @@ def getText(filename):
     for para in doc.paragraphs:
         fullText.append(para.text)
     return '\n'.join(fullText)
+
+
+def percentage(count, total):
+    return 100 * count / total;
+
+def lexical_diversity(text):
+    return len(set(text)) / len(text)
+
+#remember to use dispersion plots for graphing
