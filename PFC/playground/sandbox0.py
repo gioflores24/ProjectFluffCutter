2f0f15db45d7f04fb3df6795a9b05d6bfa571360
giovanniflores <gdr943@gmail.com>
2018-05-31 11:58:19 -0700

    Worked on new examples

--- PFC/playground/sandbox0.py
+++ PFC/playground/sandbox0.py
@@ -2,8 +2,10 @@ from nltk.book import text4
 from nltk.draw.dispersion import dispersion_plot
 from matplotlib import *
 from numpy import *
+from nltk.probability import FreqDist
 
-text = "The citizens of democracy are rejoicing in freedom and their duties are ok"
+fdist1 = FreqDist(text4)
+print(fdist1)
 
 
 
