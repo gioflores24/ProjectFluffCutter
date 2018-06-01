62857ad68e36e1a195d342d25d9d7ca22898862e
Giovanni Flores <gdr943@gmail.com>
2018-05-31 21:50:12 -0700

    Modified process

--- PFC/utils/process.py
+++ PFC/utils/process.py
@@ -6,6 +6,7 @@ def fluffCutter(tokenized):
     try:
         words = []
         for i in tokenized:
+
             words = word_tokenize(i)
             # tagged = nltk.pos_tag(words)
             # namedEnt = nltk.ne_chunk(tagged, binary=True)
