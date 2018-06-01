db2f3330f94064f156f412e611e8f92a6262bef6
Giovanni Flores <gdr943@gmail.com>
2018-05-31 19:36:42 -0700

    Reorganized src files

---
+++ PFC/playground/practice.py
@@ -0,0 +1,32 @@
+import nltk
+from nltk.tokenize import PunktSentenceTokenizer
+from nltk.corpus import state_union
+import tkinter
+from tkinter import filedialog
+from utils.preprocess import getText
+
+
+
+
+def process(tokenized):
+    try:
+        for i in tokenized:
+            words = nltk.word_tokenize(i)
+            # tagged = nltk.pos_tag(words)
+            # namedEnt = nltk.ne_chunk(tagged, binary=True)
+            # namedEnt.draw()
+        return words
+    except Exception as ex:
+        print(str(ex))
+
+def main():
+    root = tkinter.Tk()
+    root.withdraw()
+    file_path = filedialog.askopenfilename()
+    train_text = state_union.raw("2006-GWBush.txt")
+    sample_text = getText(file_path)
+    custom_sentence_tokenizer = PunktSentenceTokenizer(train_text)
+    t = custom_sentence_tokenizer.tokenize(sample_text)
+    process(t)
+
+main()
---
+++ PFC/utils/process.py
@@ -0,0 +1,15 @@
+from nltk.tokenize import word_tokenize
+
+
+
+def fluffCutter(tokenized):
+    try:
+        words = []
+        for i in tokenized:
+            words = word_tokenize(i)
+            # tagged = nltk.pos_tag(words)
+            # namedEnt = nltk.ne_chunk(tagged, binary=True)
+            # namedEnt.draw()
+        return words
+    except Exception as ex:
+        print(str(ex))
